569e90591a42471fc53444a5b3c7a93cd3575972
dipesh663 <dipeshthapa663@gmail.com>
2026-05-22 13:38:53 +0545

    Add profile image validation and error handling in user registration controller

--- BidSphere_BE/controllers/userController.js
+++ BidSphere_BE/controllers/userController.js
@@ -1,4 +1,20 @@
+import ErrorHandler from "../middlewares/error.js";
+
 export const register = () => {
     if(!req.files || Object.keys(req.files).length === 0) {
+        return next(new ErrorHandler("Profile Image Required.", 400));
     }
+
+    const {profileImage} = req.files;
+
+    const allowedFormates = ["image/png", "image/jpg", "image/webp"];
+    if (!allowedFormates.includes(profileImage.mimetype)){
+        return next(new ErrorHandler("File format not supported.", 400));
+    }
+
+    const{
+        userName,
+        email,password, phone, address, role, bankAccountnumber, bankAccountHolderName, bankName,
+        esewaAccountNumber, khaltiAccountNumber, paypalEmail
+    } = req.body;
 };

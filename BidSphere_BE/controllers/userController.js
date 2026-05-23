26d70a8e70c85e0e80e13bab148b68ca3e5122ff
dipesh663 <dipeshthapa663@gmail.com>
2026-05-23 11:37:00 +0545

    Implement user registration with profile image upload and validation

--- BidSphere_BE/controllers/userController.js
+++ BidSphere_BE/controllers/userController.js
@@ -1,13 +1,15 @@
 import ErrorHandler from "../middlewares/error.js";
+import { User } from "../models/userSchema.js";
+import {v2 as cloudinary} from "cloudinary";
 
-export const register = () => {
+export const register = async(req, res, next) => {
     if(!req.files || Object.keys(req.files).length === 0) {
         return next(new ErrorHandler("Profile Image Required.", 400));
     }
 
     const {profileImage} = req.files;
 
-    const allowedFormates = ["image/png", "image/jpg", "image/webp"];
+    const allowedFormates = ["image/png", "image/jpeg", "image/webp"];
     if (!allowedFormates.includes(profileImage.mimetype)){
         return next(new ErrorHandler("File format not supported.", 400));
     }
@@ -17,4 +19,72 @@ export const register = () => {
         email,password, phone, address, role, bankAccountnumber, bankAccountHolderName, bankName,
         esewaAccountNumber, khaltiAccountNumber, paypalEmail
     } = req.body;
+
+    if(!userName || !email || !password || !role || !address){
+        return next(new ErrorHandler("please fill full form.", 400));
+    }
+    if(role === "Auctioneer"){
+        if(!bankAccountnumber || !bankAccountHolderName || !bankName){
+            return next(
+                new ErrorHandler("please fill full form.", 400)
+            );       
+        }
+    }
+    if(!esewaAccountNumber){
+            return next(
+                new ErrorHandler("please provide your esewaAccountNumber.", 400)
+            );       
+    }
+    if(!paypalEmail){
+        return next(
+            new ErrorHandler("please provide your paypalEmail.", 400)
+        );       
+    }
+
+    const isRegistered = await User.findOne({email});
+    if(isRegistered){
+        return next(new ErrorHandler("User already registered.", 400));
+    }
+    const cloudinaryResponse = await cloudinary.uploader.upload(profileImage.tempFilePath,{
+        folder: "BidSphere/Profiles",
+    });
+    if (!cloudinaryResponse || cloudinaryResponse.error){
+        console.error("Cloudinary upload error:", 
+            cloudinaryResponse.error || "Unknown cloudinary error.");
+        return next(new ErrorHandler("Failed to upload profile image to Cloudinary.", 500));
+    }
+
+    const user = await User.create({
+        userName,
+        email,
+        password,
+        phone,
+        address,
+        role,
+        profileImage:{
+            public_id: cloudinaryResponse.public_id,
+            url: cloudinaryResponse.secure_url,
+        },
+        paymentMethod: {
+            bankTransfer: {
+                bankAccountnumber,
+                bankAccountHolderName,
+                bankName
+            },
+            eSewa: {
+                esewaAccountNumber
+            },
+            khalti: {
+                khaltiAccountNumber
+            },
+            paypal: {
+                paypalEmail
+            },
+        },
+    });
+
+    res.status(201).json({
+        success: true,
+        message: "User registered successfully.",
+    });
 };

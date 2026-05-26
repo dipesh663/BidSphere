1a93892c2b3ca9926d92aec9597c03f8be56becf
dipesh663 <dipeshthapa663@gmail.com>
2026-05-26 19:53:10 +0545

    Add password comparison and JWT token generation methods to user schema

--- BidSphere_BE/models/userSchema.js
+++ BidSphere_BE/models/userSchema.js
@@ -85,5 +85,15 @@ userSchema.pre("save", async function (next) {
     this.password = await bcrypt.hash(this.password, 10);
 });
 
+userSchema.methods.comparePassword = async function (enteredPassword){
+    return await bcrypt.compare(enteredPassword, this.password);
+};
+
+userSchema.methods.generateToken = function (){
+    return jwt.sign({id: this._id}, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_EXPIRES_IN,
+    });
+};
+
 export const User = mongoose.model("User", userSchema);
 

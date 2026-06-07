b6e67e0535d717cc4de913293515ea908c2356d4
dipesh663 <dipeshthapa663@gmail.com>
2026-06-07 20:47:47 +0545

    function created to cheack if auction already exist or not

--- BidSphere_BE/controllers/auctionItemController.js
+++ BidSphere_BE/controllers/auctionItemController.js
@@ -2,6 +2,7 @@ import { Auction} from '../models/auctionSchema.js';
 import { User } from '../models/userSchema.js';
 import { catchAsyncErrors } from '../middlewares/catchAsyncErrors.js';
 import ErrorHandler from '../utils/errorHandler.js';
+import {v2 as cloudinary} from 'cloudinary';
 
 export const addNewAuctionItem = catchAsyncErrors(async (req, res, next) => {
     if(!req.files || Object.keys(req.files).length === 0) {
@@ -29,7 +30,42 @@ export const addNewAuctionItem = catchAsyncErrors(async (req, res, next) => {
             ("Auction starting time must be less than end time.", 400));
     }
 
-    
-
-
+    const alreadyOneAuctionActive = await Auction.find({
+        createdBy: req.user._id,
+        endTime: { $gt: new Date() },
+    });
+    if (alreadyOneAuctionActive) {
+        return next(new ErrorHandler("You already have an active auction.")
+    );
+    }
+    try {
+        const cloudinaryResponse = await cloudinary.uploader.upload(image.tempFilePath,{
+        folder: "BidSphere_Auctions_images",
+    });
+    if (!cloudinaryResponse || cloudinaryResponse.error){
+        console.error("Cloudinary upload error:", 
+            cloudinaryResponse.error || "Unknown cloudinary error.");
+        return next(new ErrorHandler("Failed to upload auction image to Cloudinary.", 500));
+    }
+    const auctionItem = await Auction.create({
+        title,
+        description,
+        category,
+        startingBid,
+        startTime,
+        endTime,
+        image: {
+            public_id: cloudinaryResponse.public_id,
+            url: cloudinaryResponse.secure_url,
+        },
+        createdBy: req.user._id,
+    });
+    return res.status(201).json({
+        success: true,
+        message: `Auction item created and will be listed on auction page at ${startTime}.`,
+        auctionItem,
+    }); 
+    } catch (error) {
+        return next(new ErrorHandler(error.message || "Failed to create auction item.", 500));
+    }
 });

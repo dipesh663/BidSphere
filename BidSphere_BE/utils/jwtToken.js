32ddf0afcc842624d9081c3b3da4a1f6ec55c3b7
dipesh663 <dipeshthapa663@gmail.com>
2026-06-12 19:43:41 +0545

    Fix JWT token generation method in user schema and update token retrieval in jwtToken utility

--- BidSphere_BE/utils/jwtToken.js
+++ BidSphere_BE/utils/jwtToken.js
@@ -1,5 +1,5 @@
 export const generateToken = (user, message, statusCode, res) => {
-    const token = user.getJwtToken();
+    const token = user.generateToken();
 
     res.status(statusCode).cookie("token", token, {
         expires: new Date(Date.now() + process.env.COOKIE_EXPIRE*24*60*60*1000),

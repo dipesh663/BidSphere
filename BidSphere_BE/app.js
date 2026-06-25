c11bc265bca63ce84ab40af4281471f8cecac3a1
dipesh663 <dipeshthapa663@gmail.com>
2026-06-25 22:15:54 +0545

    Add cron job imports and initialize auction processes in app.js

--- BidSphere_BE/app.js
+++ BidSphere_BE/app.js
@@ -10,6 +10,8 @@ import auctionItemRouter from "./router/auctionItemRoutes.js";
 import bidRouter from "./router/bidRoutes.js"
 import commissionRouter from "./router/commissionRouter.js";
 import superAdminRouter  from "./router/superAdminRoutes.js";
+import { endedAuctionCron } from "./automation/endedAuctionCron.js"
+import { verifyCommissionCron } from "./automation/verifyCommissionCron.js"
 
 const app = express();
 config({
@@ -39,6 +41,8 @@ app.use("/api/v1/bid", bidRouter);
 app.use("/api/v1/commission", commissionRouter);
 app.use("/api/v1/superadmin", superAdminRouter);
 
+endedAuctionCron();
+verifyCommissionCron();
 connectDB();
 
 app.use(errorMiddleware);
